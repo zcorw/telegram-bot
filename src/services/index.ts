518c616908fb3a03dea0f1782368d3367faeb123
zcrow <zhiwei.zhang@xinyartech.com>
2021-04-14 17:52:56 +0800

    fix bug

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,34 +1,42 @@
 import { UserServiceType } from '@/globel';
-import axios, {AxiosResponse} from 'axios';
+import axios, {AxiosResponse, AxiosInstance} from 'axios';
 
-const instance = axios.create({
-  baseURL: process.env.FORM_HOST + 'user',
-  proxy: process.env.NODE_ENV === 'development' ? {
-    host: '127.0.0.1',
-    port: 8888,
-  } : undefined,
-})
-
-instance.interceptors.response.use(
-  (res: AxiosResponse<any>) => {
-    const {
-      data,
-      status
-    } = res;
-    if (
-      data.code !== 200 &&
-      data.code !== 0 &&
-      res.headers['content-type'] !== 'application/octet-stream'
-    ) {
-      return Promise.reject(data);
-    }
-    return res;
-  },
-  error => {
-    return Promise.reject(error);
+let instance: AxiosInstance;
+const createInstance = () => {
+  if (instance) {
+    return instance;
   }
-);
+  instance = axios.create({
+    baseURL: process.env.FORM_HOST + 'user',
+    proxy: process.env.NODE_ENV === 'development' ? {
+      host: '127.0.0.1',
+      port: 8888,
+    } : undefined,
+  })
+  
+  instance.interceptors.response.use(
+    (res: AxiosResponse<any>) => {
+      const {
+        data,
+        status
+      } = res;
+      if (
+        data.code !== 200 &&
+        data.code !== 0 &&
+        res.headers['content-type'] !== 'application/octet-stream'
+      ) {
+        return Promise.reject(data);
+      }
+      return res;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+  return instance;
+}
+
 
 export const addUser = (data: UserServiceType) => {
-  return instance.post('/add/book', data);
+  return createInstance().post('/add/book', data);
 }
--- src/services/register.ts
+++ src/services/register.ts
@@ -1,24 +1,33 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosInstance } from 'axios';
 
-const instance = axios.create({
-  baseURL: `http://127.0.0.1:${process.env.PORT2}`,
-  proxy: process.env.NODE_ENV === 'development' ? {
-    host: '127.0.0.1',
-    port: 8888,
-  } : undefined,
-});
-
-instance.interceptors.response.use(
-  (res: AxiosResponse<any>) => {
-    return res;
-  },
-  error => {
-    return Promise.reject(new Error(error.response?.data || error.code));
+let instance: AxiosInstance;
+const createInstance = () => {
+  if (instance) {
+    return instance;
   }
-);
+  instance = axios.create({
+    baseURL: `http://127.0.0.1:${process.env.PORT2}`,
+    proxy: process.env.NODE_ENV === 'development' ? {
+      host: '127.0.0.1',
+      port: 8888,
+    } : undefined,
+  });
+  instance.interceptors.response.use(
+    (res: AxiosResponse<any>) => {
+      return res;
+    },
+    error => {
+      return Promise.reject(new Error(error.response?.data || error.code));
+    }
+  );
+  return instance;
+}
+
+
+
 
 export const register = (adminName, adminPassw, userName) => {
-  return instance.get('/book/login', {
+  return createInstance().get('/book/login', {
     params: {
       username: adminName,
       password: adminPassw,
